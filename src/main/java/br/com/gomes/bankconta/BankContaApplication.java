1e540a0d6620c6c278ad39ad2122a72c1d4a539e
guigomes91 <guilherme.gomes91@outlook.com>
2023-11-19 18:11:30 -0300

    first commit

---
+++ src/main/java/br/com/gomes/bankconta/BankContaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gomes.bankconta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankContaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankContaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gomes/bankconta/BankContaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gomes.bankconta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankContaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
