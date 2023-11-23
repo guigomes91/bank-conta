d77efd26c1471523d35f94edbacb78bfb82b3e5e
guigomes91 <guilherme.gomes91@outlook.com>
2023-11-22 21:43:55 -0300

    Add pacotes

--- src/main/java/br/com/gomes/bankconta/BankContaApplication.java
+++ src/main/java/br/com/gomes/bankconta/BankContaApplication.java
@@ -1,13 +1,22 @@
 package br.com.gomes.bankconta;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
-public class BankContaApplication {
+public class BankContaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankContaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Caiu aqui...");
 	}
 
 }
