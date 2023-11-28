b25c991b0803af3e41cfd845094ef03a9ffb808a
guigomes91 <guilherme.gomes91@outlook.com>
2023-11-27 22:19:06 -0300

    Ajuste na classe BankContaApplicatio

--- src/main/java/br/com/gomes/bankconta/BankContaApplication.java
+++ src/main/java/br/com/gomes/bankconta/BankContaApplication.java
@@ -1,22 +1,12 @@
 package br.com.gomes.bankconta;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @SpringBootApplication
-public class BankContaApplication implements CommandLineRunner {
+public class BankContaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankContaApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		log.info("Caiu aqui...");
-	}
-
 }
