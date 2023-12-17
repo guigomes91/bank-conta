489750ed9d7106b8827b903d33afc479e8e66cbc
guigomes91 <guilherme.gomes91@outlook.com>
2023-12-17 14:24:29 -0300

    Conta poupanca controller

---
+++ src/main/java/br/com/gomes/bankconta/controller/ContaPoupancaController.java
@@ -0,0 +1,10 @@
+package br.com.gomes.bankconta.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/contapoupanca")
+public class ContaPoupancaController {
+
+}
