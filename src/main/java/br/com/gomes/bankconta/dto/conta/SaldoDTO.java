a215f8fa6e7294a1020bda5866675941312894e0
guigomes91 <guilherme.gomes91@outlook.com>
2024-01-10 19:36:47 -0300

    Add atributos para SaldoDTO

--- src/main/java/br/com/gomes/bankconta/dto/conta/SaldoDTO.java
+++ src/main/java/br/com/gomes/bankconta/dto/conta/SaldoDTO.java
@@ -1,5 +1,19 @@
 package br.com.gomes.bankconta.dto.conta;
 
+import java.math.BigDecimal;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class SaldoDTO {
 
+	Long numeroConta;
+	BigDecimal saldo;
 }
