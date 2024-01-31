0f7807411070df67320ee6056a45911d6cee9930
guigomes91 <guilherme.gomes91@outlook.com>
2024-01-31 19:05:08 -0300

    Método lancarMovimento em movimentoContaPoupancaComponent

--- src/main/java/br/com/gomes/bankconta/components/MovimentoContaPoupancaComponent.java
+++ src/main/java/br/com/gomes/bankconta/components/MovimentoContaPoupancaComponent.java
@@ -2,19 +2,27 @@ package br.com.gomes.bankconta.components;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.gomes.bankconta.dto.movimento.MovimentoInputDTO;
 import br.com.gomes.bankconta.dto.movimento.MovimentoOutputDTO;
 import br.com.gomes.bankconta.entities.conta.Conta;
+import br.com.gomes.bankconta.entities.movimento.MovimentoContaPoupancaEntity;
+import br.com.gomes.bankconta.repository.MovimentoContaPoupancaRepository;
 import br.com.gomes.bankconta.service.impl.Operacao;
 
 @Component
 public class MovimentoContaPoupancaComponent implements Operacao {
 
+	@Autowired
+	private MovimentoContaPoupancaRepository contaPoupancaRepository;
+	
 	@Override
 	public MovimentoOutputDTO lancarMovimento(MovimentoInputDTO movimento) {
-		return null;
+		MovimentoContaPoupancaEntity movimentoContaPoupancaEntity = contaPoupancaRepository.save(MovimentoContaPoupancaEntity.dtoToEntity(movimento));
+		
+		return MovimentoOutputDTO.entityToDto(movimentoContaPoupancaEntity);
 	}
 
 	@Override
