bedf9790aebf04c71b0b5fb250d768b96b0d362d
guigomes91 <guilherme.gomes91@outlook.com>
2024-03-04 19:44:32 -0300

    Eureka client para o serviço se registrar no server discovery

--- src/main/java/br/com/gomes/bankconta/BankContaApplication.java
+++ src/main/java/br/com/gomes/bankconta/BankContaApplication.java
@@ -2,8 +2,10 @@ package br.com.gomes.bankconta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BankContaApplication {
 
 	public static void main(String[] args) {
